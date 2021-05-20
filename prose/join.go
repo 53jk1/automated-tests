cc251bee1231ddd4807962618f086d098dd56c16
Kacper Bąk <kacper.bak@sonderfin.com>
2021-05-20 22:19:47 +0200

    v0.2

--- prose/join.go
+++ prose/join.go
@@ -4,7 +4,7 @@ import "strings"
 
 func JoinWithCommas(phrases []string) string {
 	result := strings.Join(phrases[:len(phrases)-1], ", ")
-	result += ", a także"
+	result += ", a także "
 	result += phrases[len(phrases)-1]
 	return result
 }
