b3c0aa8b21c40d15f2d005a4fd009d173328b702
Kacper Bąk <kacper.bak@sonderfin.com>
2021-05-20 22:43:29 +0200

    v0.3

---
+++ prose/join_test.go
@@ -0,0 +1,11 @@
+package prose
+
+import "testing"
+
+func TestTwoElements(t *testing.T) {
+	t.Error("Test nie został jeszcze napisany.")
+}
+
+func TestThreeElements(t *testing.T) {
+	t.Error("Także brak testu.")
+}
