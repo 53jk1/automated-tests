8b75eaba810b363c8ce4e8ba7fe05676c673b1f8
Kacper Bąk <kacper.bak@sonderfin.com>
2021-05-20 22:19:20 +0200

    v0.1

--- join/main.go
+++ join/main.go
@@ -1,4 +1,14 @@
 package main
+
 import (
-	
+	"fmt"
+
+	"github.com/53jk1/automated-tests/prose"
 )
+
+func main() {
+	phrases := []string{"moi rodzice", "klaun z rodeo"}
+	fmt.Println("Zdjęcie, na którym są", prose.JoinWithCommas(phrases))
+	phrases = []string{"moi rodzice", "klaun z rodeo", "nagrodzony byk"}
+	fmt.Println("Zdjęcie, na którym są", prose.JoinWithCommas(phrases))
+}
